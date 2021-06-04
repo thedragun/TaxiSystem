cd1895ad67646d76d1113aed677830637f765b1d
thedragun <western.jake.morris@gmail.com>
2021-06-04 14:52:48 +1200

    added files and line function.

--- TaxiSystem.cpp
+++ TaxiSystem.cpp
@@ -2,11 +2,17 @@
 #include <string>
 #include <fstream>
 #include <time.h>
+
 using namespace std;
+
+void line() {
+	for (int i = 0; i != 66; i++) {
+		cout << "*";
+	}
+}
 
 int main()
 {
-	cout << "hi";
 
 
 
