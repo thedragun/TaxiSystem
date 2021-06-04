72730e640603e787c9451297e5c4543bcbe6bcb5
Joel Simpson <270112350@yoobeestudent.ac.nz>
2021-06-04 15:23:46 +1200

    terms and conditions

--- TaxiSystem.cpp
+++ TaxiSystem.cpp
@@ -4,6 +4,56 @@
 #include <time.h>
 using namespace std;
 
+void printTerms() {
+	char ans;
+	cout << "\n\nTerms and Conditions :\n\n";
+	cout << "These are the Terms and Conditions governing the use of this Service and the agreement that operates between\nYou and the Company. \n";
+	cout << "These Terms and Conditions set out the rights and obligations of all users regarding the use of the Service. \n";
+	cout << "Your access to and use of the Service is conditioned on Your acceptance of and compliance with these \nTerms and Conditions.\n";
+	cout << "These Terms and Conditions apply to all visitors, users and others who access or use the Service.\n";
+	cout << "Your access to and use of the Service is also conditioned on Your acceptance of and compliance with the \nPrivacy Policy of the Company.\n";
+	cout << "Our Privacy Policy describes Our policies and procedures on the collection,\n";
+	cout << "use and disclosure of Your personal information when You use the Application or the Website and tells\nYou about Your privacy rights and how the law protects You.\n";
+	cout << "We reserve the right, at Our sole discretion, to modify or replace these Terms at any time.\n";
+	cout << "If a revision is material We will make reasonable efforts to provide at least 30 days' notice prior to any\nnew terms taking effect. What constitutes a material change will be determined at Our sole discretion.\n";
+	cout << "We own all your assets you if you use this Program.\n\n";
+	cout << "*************************************************************************************************************\n";
+	reans:
+	cout << "\nAccept the Terms and Conditions? (Y or N) : ";	
+	cin >> ans;
+	if (ans == 'Y' || ans == 'y') {
+		cout << "Thank You for Accepting!\n";
+	}
+	else if (ans == 'N' || ans == 'n') {
+		cout << "\nPlease Accept The Terms to Continue...\n";
+		goto reans;
+	}
+	else {
+		cout << "\nPlease Enter a valid Answer (Y or N).\n";
+		goto reans;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main()
 {
 	
@@ -51,7 +101,8 @@ int main()
 	
 	
 	int ans;
-	cout << "Taxi Trip Booking System\n\n";
+	rerun:
+	cout << "\nTaxi Trip Booking System\n\n";
 	cout << "*************************************\n";
 	cout << "             Only Trips\n";
 	cout << "*************************************\n\n";
@@ -66,20 +117,20 @@ int main()
 
 	switch (ans) {
 	case 1 :
-
-		break;
+		printTerms();
+		goto rerun;
 
 	case 2 : 
 
-		break;
+		goto rerun;
 
 	case 3 :
 
-		break;
+		goto rerun;
 
 	case 4 :
 
-		break;
+		goto rerun;
 
 	case 5 : 
 
